9095c6bed0bc2b28df99f7ae069bf7160f5bdef4
Reginald Shawn Annan <shawnrannan@gmail.com>
2024-02-20 09:22:51 +0000

    add dropdown menu animations.

--- src/components/Navbar/DropDownMenu.vue
+++ src/components/Navbar/DropDownMenu.vue
@@ -1,8 +1,9 @@
 <template>
-  <hr class="block w-svw dropDownMenu" />
+  <hr class="block w-svw dropDownMenu lg:" />
+
   <div class="block w-svw bg-reddish-100 p-4 dropDownMenu">
     <ul class="flex flex-col items-start gap-5">
-      <li class="li-link">About Me</li>
+      <li class="li-link" @click="console.log(showMenu)">About Me</li>
       <li class="li-link">Personal Projects</li>
       <li class="li-link">Contact Me</li>
     </ul>
@@ -10,12 +11,7 @@
 </template>
 
 <script>
-import { dropDownMenu, dropDownMenuOut } from "@/animations/NavbarAnimations";
-import { onBeforeUnmount, onMounted } from "vue";
 export default {
-  setup() {
-    onMounted(() => dropDownMenu());
-    onBeforeUnmount(() => dropDownMenuOut());
-  },
+  setup() {},
 };
 </script>
--- src/components/Navbar/Navbar.vue
+++ src/components/Navbar/Navbar.vue
@@ -8,9 +8,11 @@
       <NavbarSVG @click="handleMenu" />
     </div>
 
-    <div v-if="showMenu">
-      <DropDownMenu />
-    </div>
+    <Transition appear @enter="onEnter" @leave="onLeave">
+      <div v-if="showMenu">
+        <DropDownMenu />
+      </div>
+    </Transition>
   </nav>
 </template>
 
@@ -18,18 +20,37 @@
 import { ref } from "vue";
 import NavbarSVG from "./NavbarSVG.vue";
 import DropDownMenu from "./DropDownMenu.vue";
-import { dropDownMenuOut } from "@/animations/NavbarAnimations";
-
+import { gsap } from "gsap";
 export default {
   components: { NavbarSVG, DropDownMenu },
   setup() {
     const showMenu = ref(false);
 
+    const onEnter = (el, done) => {
+      const tl = gsap.timeline({ duration: 0.5, paused: true });
+      tl.from(el, {
+        x: 1024,
+        ease: "circ",
+      });
+      tl.from(".li-link", {
+        opacity: 0.2,
+      });
+      tl.play();
+    };
+    const onLeave = (el, done) => {
+      gsap.to(el, {
+        duration: 1,
+        x: 1024,
+        ease: "sine",
+        onComplete: done,
+      });
+      // console.log("animation done");
+    };
     const handleMenu = async () => {
       showMenu.value = !showMenu.value;
     };
 
-    return { showMenu, handleMenu };
+    return { showMenu, handleMenu, onEnter, onLeave };
   },
 };
 </script>
